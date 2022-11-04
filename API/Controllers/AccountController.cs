09a08a6e7f8ae9ed98cb3b2ccae58d29434e2e82
Subhabroto Roy Choudhury <suvo63@gmail.com>
2022-11-04 18:23:34 +0530

    End of section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext context;
+        private readonly ITokenService tokenService;
+
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            this.context = context;
+            this.tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+            using var hmac=new HMACSHA512();
+            
+            var user=new AppUser{
+                Username=registerDto.Username,
+                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt=hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return new UserDto{
+                Username=user.Username,
+                Token=tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")]
+
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user= await context.Users
+                .SingleOrDefaultAsync(x=>x.Username==loginDto.Username);
+            
+            if (user==null) return Unauthorized("Invalid username");
+
+            using var hmac=new HMACSHA512(user.PasswordSalt);
+
+            var computedHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i=0;i<computedHash.Length;i++){
+                if (computedHash[i]!=user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return new UserDto{
+                Username=user.Username,
+                Token=tokenService.CreateToken(user)
+            };
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x=>x.Username==username.ToLower());
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Text;
+using API.Extentions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -28,12 +18,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options=>
-            {
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
+            services.AddApplicationServices(Configuration);
             services.AddControllers();
             services.AddCors();
+            services.AddIdentityServices(Configuration);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -54,6 +42,7 @@ namespace API
 
             app.UseRouting();
             app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
